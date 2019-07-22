239cba7875cec5142a3b9ec1b8cb601264739b2f
mboslin <michael@boslin.com>
2019-07-22 11:09:17 -0700

    Added starting jquery to js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,23 @@
-$( document ).ready(function(){
 
-var num1 = Math.floor(Math.random() * 11 + 1)
 
-var num2 = Math.floor(Math.random() * 11 + 1)
+$(document).ready(function () {
 
-var num3 = Math.floor(Math.random() * 11 + 1)
+    var Random = Math.floor(Math.random() * 101 + 19)
 
-var num4 = Math.floor(Math.random() * 11 + 1)
+    var num1 = Math.floor(Math.random() * 11 + 1)
 
-var userTotal = 0;
-var wins = 0;
-var losses = 0;
+    var num2 = Math.floor(Math.random() * 11 + 1)
+
+    var num3 = Math.floor(Math.random() * 11 + 1)
+
+    var num4 = Math.floor(Math.random() * 11 + 1)
+
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+
+
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
 
 }
