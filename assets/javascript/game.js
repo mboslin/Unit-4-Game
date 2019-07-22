cb37a15194e755a52c4b4b7164e1935786850a1e
mboslin <michael@boslin.com>
2019-07-22 16:40:08 -0700

    added jquery on click function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,31 +16,60 @@ $(document).ready(function () {
     var wins = 0;
     var losses = 0;
 
+}
 
     $('#numberWins').text(wins);
     $('#numberLosses').text(losses);
 
 
-    function win() {
-        alert("You are so SMART!! You WON!");
-        wins++;
-        $('#numberWins').text(wins);
-        reset();
+function win() {
+
+    alert("You are so SMART!! You WON!");
+    wins++;
+
+    $('#numberWins').text(wins);
+    reset();
+}
+
+
+function lose() {
+
+    alert("Try Harder next time.. You LOSE!");
+    losses++;
+
+    $('#numberLosses').text(losses);
+    reset()
+}
+
+
+$('#one').on('click', function () {
+
+    userTotal = userTotal + num1;
+
+    console.log("New userTotal= " + userTotal);
+
+    $('#finalTotal').text(userTotal);
+
+
+    if (userTotal == Random) {
+        win();
     }
 
-
-    function lose() {
-        alert("Try Harder next time.. You LOSE!");
-        losses++;
-        $('#numberLosses').text(losses);
-        reset()
+    else if (userTotal > Random) {
+        lose();
     }
+})  
+    
+$('#two').on ('click', function(){
+    userTotal = userTotal + num2;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+        if (userTotal == Random){
+          win();
+        }
+        else if ( userTotal > Random){
+          lose();
+        } 
+  })  
 
-    $('#one').on('click', function () {
-        userTotal = userTotal + num1;
-
-
-
-
-    }
 }
