701915ee1e4af420754e0e38107c17632ca8e93b
mboslin <michael@boslin.com>
2019-07-21 23:27:41 -0700

    Started ready function to js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
-var Random = Math.floor(Math.random() * 101 + 19)
-
-
-$('#randomNumber').text(Random);
-
+$( document ).ready(function(){
 
 var num1 = Math.floor(Math.random() * 11 + 1)
+
 var num2 = Math.floor(Math.random() * 11 + 1)
+
 var num3 = Math.floor(Math.random() * 11 + 1)
+
 var num4 = Math.floor(Math.random() * 11 + 1)
 
 var userTotal = 0;
 var wins = 0;
 var losses = 0;
 
+}
